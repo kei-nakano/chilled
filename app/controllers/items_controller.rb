2a050b66e8d39071c8ec087911f61ab2d0974271
EC2 Default User <ec2-user@ip-172-31-41-119.us-east-2.compute.internal>
2020-05-05 22:34:32 +0000

    Add create/delete/update to items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,34 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def show
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(title: params[:item][:title])
+    item.save
+    redirect_to("/items")
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.title = params[:item][:title]
+    redirect_to("/items") if item.save
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    redirect_to("/items") if item.destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'home#top'
   get 'about' => 'home#about'
-  get 'items/index' => 'items#index'
+  # get 'items/index' => 'items#index'
+  # get 'items/:id' => 'items#show'
+  # get 'items/new' => 'items#new'
+  # post 'items' => 'items#create'
+  # get 'items/:id/edit' => "items#edit"
+  # patch 'items/:id' => 'item#update'
+  # delete 'items/:id' => 'item#destroy'
+  resources :items
 end
