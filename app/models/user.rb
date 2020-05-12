cc135ea00de7d088756c809d98adcee6669afcfa
kei-nakano <keisukenever4get@gmail.com>
2020-05-12 09:36:17 +0000

    パスワード暗号化を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  before_save { self.email = email.downcase }
   mount_uploader :image, ImageUploader
   validate :image_size
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password(validations: false)
 
   # DBのリレーション定義後に削除予定
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :model do
-  describe 'validates :name, presence: true' do
-    context 'nameが存在しない' do
-      example 'valid?がfalseになる' do
-        user = User.create(name: "", email: "test@gmail.com", password: "test")
-        expect(user.errors.full_messages.count).to eq 1
-        expect(user.errors.full_messages).to eq ["Name can't be blank"]
+  describe 'validates presence of name ' do
+    context 'no name' do
+      it 'is not valid' do
+        user = User.create(name: "", email: "test@gmail.com", password: "12345678")
+        expect(user).not_to be_valid
+        expect(user.errors).to be_added(:name, :blank)
       end
     end
-    context 'emailが存在しない' do
-      example 'valid?がfalseになる' do
-        user = User.new(name: "test", email: "", password: "test")
-        expect(user.errors.full_messages.count).to eq 1
-        expect(user.errors.full_messages).to be "Name can't be blank"
+
+    context 'name is present' do
+      it 'is valid' do
+        user = User.create(name: "taka", email: "test@gmail.com", password: "12345678")
+        expect(user).to be_valid
       end
     end
   end
