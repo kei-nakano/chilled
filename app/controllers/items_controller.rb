ae68f9d17243f2522e1325187d3d34890d24983e
kei-nakano <keisukenever4get@gmail.com>
2020-05-21 19:17:06 +0000

    item/show画面の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @reviews = Review.where(item_id: @item.id)
+    @reviews = @item.reviews
   end
 
   def new
