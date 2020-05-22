d788170ce44083e701d733d97355a0c28c03388c
kei-nakano <keisukenever4get@gmail.com>
2020-05-22 18:05:24 +0000

    非同期コメント機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :reviews, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   # ユーザーをフォローする
   def follow(other_user)
