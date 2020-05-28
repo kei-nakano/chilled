bc3f80f7191ba3699fe788d5e3c80824c442189b
kei-nakano <keisukenever4get@gmail.com>
2020-05-28 17:25:30 +0000

    マイグレーションファイルの修正

--- db/migrate/20200505060000_create_users.rb
+++ db/migrate/20200505060000_create_users.rb
@@ -3,8 +3,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :image
+      t.string :password_digest
 
       t.timestamps
+    end
+    change_table :users, bulk: true do |t|
+      t.index :email, unique: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_05_28_130663) do
     t.integer "calorie"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["manufacturer_id"], name: "index_items_on_manufacturer_id"
   end
@@ -114,10 +113,10 @@ ActiveRecord::Schema.define(version: 2020_05_28_130663) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "image"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
