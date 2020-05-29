781f15a286f88812768554c99a81812e48ef6a9c
kei-nakano <keisukenever4get@gmail.com>
2020-05-29 18:24:42 +0000

    searchメソッドを定義

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,7 +4,19 @@ class ApplicationRecord < ActiveRecord::Base
   def self.search(keyword)
     class_name = name
     search = "%" + keyword + "%"
-    return where('name like ?', search) if class_name == "Item"
-    # return where('name like ?', search) if class_name == "Category" || "Manufacturer" || "User"
+    return where('content like ?', search) if class_name == "Review"
+    return where('name like ?', search) if class_name == "Category"
+    return where('name like ?', search) if class_name == "Manufacturer"
+    return where('name like ?', search) if class_name == "User"
+
+    if class_name == "Item"
+      return Item.eager_load(:manufacturer, :category, :tags).where('items.title like ? or
+                                                             items.content like ? or
+                                                             manufacturers.name like ? or
+                                                             categories.name like ? or
+                                                             tags.name like ?', search, search, search, search, search)
+    end
+    pluck(:name)
+    # where('name like ?', search)
   end
 end
