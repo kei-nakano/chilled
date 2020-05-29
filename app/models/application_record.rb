0c29749833cf224a4322819a771b82aa9a6ed5a6
kei-nakano <keisukenever4get@gmail.com>
2020-05-29 13:07:46 +0000

    検索用のsearchメソッドを定義

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.search(keyword)
+    class_name = name
+    search = "%" + keyword + "%"
+    return where('name like ?', search) if class_name == "Item"
+    # return where('name like ?', search) if class_name == "Category" || "Manufacturer" || "User"
+  end
 end
