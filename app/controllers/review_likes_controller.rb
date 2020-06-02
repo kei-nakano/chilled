e31864e0f93917d2f1df64680fdf3407541afa50
kei-nakano <keisukenever4get@gmail.com>
2020-06-02 11:53:57 +0000

    reviewに対するいいね機能を実装

---
+++ app/controllers/review_likes_controller.rb
@@ -0,0 +1,21 @@
+class ReviewLikesController < ApplicationController
+  def create
+    @review = Review.find(params[:review_id])
+    ReviewLike.create(user_id: @current_user.id, review_id: @review.id)
+    @count = params[:count].to_i + 1
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:review_id])
+    ReviewLike.find_by(user_id: @current_user.id, review_id: params[:review_id]).destroy
+    @count = params[:count].to_i - 1
+
+    respond_to do |format|
+      format.js
+    end
+  end
+end
