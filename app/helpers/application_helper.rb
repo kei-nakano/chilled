f7bf4a00bc50fccfed38d3279fd1a440feef4022
kei-nakano <keisukenever4get@gmail.com>
2020-06-02 17:00:45 +0000

    tweetURL用のhelperを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # tweet用のurlを生成
+  def tweet_url(url, review_id)
+    if url.include?('=')
+      position = url.index('=')
+      url.slice(0..position) + review_id.to_s
+    else
+      url + "?review_id=#{review_id}"
+    end
+  end
 end
