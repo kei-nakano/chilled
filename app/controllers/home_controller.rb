199d344bfac3ea7569928067369962ed9598b490
kei-nakano <keisukenever4get@gmail.com>
2020-06-15 18:57:43 +0000

    topページのviewを修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def top
-    @reviews = Review.where.not(id: @current_user.block_ids)
-    @items = Item.all
+    review_select = 7
+    item_select = 10
+    @reviews = Review.where.not(id: @current_user.block_ids).limit(review_select)
+    @items = Item.all.limit(item_select)
     @digest_length = 60
     @most_used_tag = ActsAsTaggableOn::Tag.order(taggings_count: :desc).first.name
   end
