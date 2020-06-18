3f6b4dead94aa6c266563c51f889b5cafda46f5a
kei-nakano <keisukenever4get@gmail.com>
2020-06-18 14:33:07 +0000

    topページのランキング機能を修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,41 @@
 class HomeController < ApplicationController
   def top
+    @digest_length = 60
     review_select = 7
     item_select = 10
-    select_period = 48 # 過去48時間以内のレビューを抽出する
-    from = Time.zone.now - select_period.hour
+    select_period = 2
+    from = Time.zone.now - select_period.day
     to = Time.zone.now
 
-    # いいねとコメントの総合スコアが高いものを抽出
+    # いいねとコメントの総合スコアが高いレビューを抽出
     popular_reviews_all = Review.where.not(id: @current_user.block_ids).order(['field(id, ?)', Review.popular_ids])
     @popular_reviews = popular_reviews_all.limit(review_select)
 
-    # 指定期間内のレビューのうち、総合スコアが高いものを抽出
+    # 指定期間内のレビューのうち、総合スコアが高いレビューを抽出
     @recent_reviews = popular_reviews_all.where(created_at: from..to).limit(review_select)
+    while @recent_reviews.count < review_select
+      select_period += 1 # 抽出数が足りない場合は、対象期間を変更する
+      from = Time.zone.now - select_period.day
+      break if select_period > 10
 
-    # スコアの合計点が高いものを抽出
-    @popular_items = Item.order(['field(id, ?)', Item.popular_ids]).limit(item_select)
-    @digest_length = 60
+      @recent_reviews = popular_reviews_all.where(created_at: from..to).limit(review_select)
+    end
+
+    # スコアの合計点が高い商品を抽出 ※※※※※※※※※※
+    @popular_items = Item.where(id: Item.popular_ids).order(['field(id, ?)', Item.popular_ids]).limit(item_select)
+
+    # 食べた！の降順に商品を抽出
+    @eaten_items = Item.order(['field(id, ?)', Item.eaten_ids]).limit(item_select)
+
+    # 食べてみたい！の降順に商品を抽出
+    @want_to_eat_items = Item.order(['field(id, ?)', Item.want_to_eat_ids]).limit(item_select)
+
+    # 1円あたりの内容量(g)が大きい商品を抽出
+    @reasonable_items = Item.order(['field(id, ?)', Item.reasonable_ids]).limit(item_select)
+
+    # カロリーの昇順に商品を抽出
+    @low_calorie_items = Item.order(calorie: :asc).limit(item_select)
+
     @most_used_tag = ActsAsTaggableOn::Tag.order(taggings_count: :desc).first.name
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,9 +18,29 @@ class Item < ApplicationRecord
     reviews.sum(:score) / count
   end
 
-  # topページのランキング用に、レビューのスコアを集計し、合計ポイント順にidを返す
+  # レビューのスコアを集計し、合計ポイント順にidを返す
   def self.popular_ids
     total_score = Review.group(:item_id).sum(:score)
     Hash[total_score.sort_by { |_, score| -score }].keys
+  end
+
+  # 内容量(g)を単価で割った値が大きい順にidを返す
+  def self.reasonable_ids
+    gram_hash = Item.pluck(:id, :gram).to_h
+    price_hash = Item.pluck(:id, :price).to_h
+    cost_performance = gram_hash.merge(price_hash) { |_key, gram, price| gram.to_f / price }
+    Hash[cost_performance.sort_by { |_, point| -point }].keys
+  end
+
+  # 食べた！の降順にidを返す
+  def self.eaten_ids
+    eaten_count = EatenItem.group(:item_id).count
+    eaten_count.sort_by { |_, v| -v }.to_h.keys
+  end
+
+  # 食べてみたい！の降順にidを返す
+  def self.want_to_eat_ids
+    want_count = WantToEatItem.group(:item_id).count
+    want_count.sort_by { |_, v| -v }.to_h.keys
   end
 end
