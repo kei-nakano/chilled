82533cb4fa4669ba6acf54461af787e9e1510279
kei-nakano <keisukenever4get@gmail.com>
2020-06-20 18:44:49 +0000

    topページの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
       url + "?review_id=#{review_id}"
     end
   end
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "冷凍食品のレビューサイト「Chill℃」"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
