692f0f9f8730bcdb68049affd6732e76df4a079a
kei-nakano <keisukenever4get@gmail.com>
2020-06-22 23:22:19 +0000

    searchメソッドの修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -9,9 +9,10 @@ class ApplicationRecord < ActiveRecord::Base
     return eager_load(:tags).where('reviews.content like ? or tags.name like ?', search, search) if class_name == "Review"
     return where('name like ?', search) if class_name.in?(%w[Category User Manufacturer]) # tagはApplicationRecordを継承していないため含めない
 
-    Item.eager_load(:manufacturer, :category).where('items.title like ? or
-                                                             items.content like ? or
-                                                             manufacturers.name like ? or
-                                                             categories.name like ?', search, search, search, search)
+    Item.eager_load(:manufacturer, :category, reviews: :tags).where('items.title like ? or
+                                                                     items.content like ? or
+                                                                     manufacturers.name like ? or
+                                                                     categories.name like ? or 
+                                                                     tags.name like ?', search, search, search, search, search)
   end
 end
