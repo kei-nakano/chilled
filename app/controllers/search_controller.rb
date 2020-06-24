28034a86999fb2846416575245aab8be05d5d3b1
kei-nakano <keisukenever4get@gmail.com>
2020-06-24 21:10:24 +0000

    userページのview修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def show
     @keyword = params[:keyword] || ""
     @keyword == "" ? (@digest = "全件の検索結果") : (@digest = "「#{@keyword}」の検索結果")
-    @type = params[:type] || "item" # 検索ボタン以外からのページ遷移直後に、itemタブを表示するようにする
+    @type = params[:type] || "item" # typeの指定がない場合、itemタブを優先表示するようにする
 
     @items = Item.search(@keyword)
     @reviews = Review.search(@keyword).where.not(user_id: block_ids(@current_user))
@@ -14,14 +14,16 @@ class SearchController < ApplicationController
     if @type == "item"
       @results = @items
       respond_to do |format|
-        return format.js
+        format.js
+        format.html
       end
+      return
     end
 
     if @type == "review"
       @results = @reviews
       respond_to do |format|
-        return format.js
+        format.js
       end
     end
 
@@ -35,8 +37,10 @@ class SearchController < ApplicationController
     if @type == "user"
       @results = @users
       respond_to do |format|
-        return format.js
+        format.js
+        format.html
       end
+      return
     end
 
     if @type == "manufacturer"
