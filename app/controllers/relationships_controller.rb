1135989b5f7b1cb3a51089f10f6e813703377fa3
kei-nakano <keisukenever4get@gmail.com>
2020-06-25 16:22:52 +0000

    userページのviewを修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user, only: %i[create destroy]
 
   def create
-    @user = User.find(params[:followed_id])
+    @user = User.find(params[:user_id])
     @current_user.follow(@user)
     @user.create_notice_follow(@current_user)
     respond_to do |format|
@@ -12,7 +12,7 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:followed_id])
+    @user = User.find(params[:id])
     @current_user.unfollow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, only: %i[index show edit update following followers]
+  before_action :authenticate_user, only: %i[show edit update]
   before_action :forbid_login_user, only: %i[new create login_form login]
   before_action :ensure_correct_user, only: %i[edit update]
-
-  def index
-    @users = User.all
-  end
 
   def login_form; end
 
@@ -30,17 +26,66 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @room_id = @current_user.room_id(@user)
-  end
+    @type = params[:type] || "review" # typeの指定がない場合、reviewタブを優先表示するようにする
 
-  def following
-    @user = User.find(params[:id])
+    if @type == "review"
+      @results = @user.reviews
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    if @type == "want_to_eat_item"
+      item_ids = "SELECT item_id FROM want_to_eat_items WHERE user_id = :user_id"
+      @results = Item.where("id IN (#{item_ids})", user_id: @user.id)
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    if @type == "eaten_item"
+      item_ids = "SELECT item_id FROM eaten_items WHERE user_id = :user_id"
+      @results = Item.where("id IN (#{item_ids})", user_id: @user.id)
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    if @type == "liked_review"
+      review_ids = "SELECT review_id FROM review_likes WHERE user_id = :user_id"
+      @results = Review.where("id IN (#{review_ids})", user_id: @user.id)
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    if @type == "following"
+      @results = @user.following
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    return unless @type == "follower"
+
+    @results = @user.followers
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 
   def blocking
-    @user = User.find(params[:id])
-  end
-
-  def followers
     @user = User.find(params[:id])
   end
 
@@ -77,11 +122,6 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:notice] = "削除しました"
     redirect_to '/users'
-  end
-
-  def likes
-    @user = User.find(params[:id])
-    @likes = Like.where(user_id: @user.id)
   end
 
   def timeline
