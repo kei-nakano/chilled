8f795fb8d3ffa9f2d525a445c3ba44290057e58e
kei-nakano <keisukenever4get@gmail.com>
2020-06-26 22:12:54 +0000

    noticeページの未読処理を追加

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,6 +1,11 @@
 class NoticesController < ApplicationController
   def index
-    @notices = @current_user.passive_notices.order(created_at: :desc).includes(:comment, :visitor, :review)
+    @notices = @current_user.passive_notices
+    @latest_notices = @notices.where(checked: false).order(created_at: :desc).includes(:comment, :visitor, :review)
+    @old_notices = @notices.where.not(id: @latest_notices.ids).order(created_at: :desc).includes(:comment, :visitor, :review)
+    @latest_notices.each do |notice|
+      notice.update(checked: true)
+    end
   end
 
   def destroy
