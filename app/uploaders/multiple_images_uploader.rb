516372ea4e0ba44242f22becb310d5b0439df6ca
kei-nakano <keisukenever4get@gmail.com>
2020-07-05 04:34:49 +0900

    upload画像を消去

--- app/uploaders/multiple_images_uploader.rb
+++ app/uploaders/multiple_images_uploader.rb
@@ -1,4 +1,4 @@
-class MultipleImageUploader < CarrierWave::Uploader::Base
+class MultipleImagesUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
