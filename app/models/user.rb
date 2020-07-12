b9e7780cfce51f80c36a6d51822f37508425d07d
kei-nakano <keisukenever4get@gmail.com>
2020-07-12 18:22:03 +0900

    gemfileの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
   before_destroy :rooms_destroy_all
--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
 # UNIXドメインソケットでの連携時、TCP通信での連携は不要になるので、port設定をコメントアウトし、tcpでのlisten停止させる
