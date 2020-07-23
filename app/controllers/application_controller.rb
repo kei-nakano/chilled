9cd41e0c0f6973ddce6bb97dfc8ab7ae6411bb8a
kei-nakano <keisukenever4get@gmail.com>
2020-07-23 10:25:19 +0900

    フレンドリーフォーワードを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,17 +16,33 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # 未ログインユーザをログインページへリダイレクトさせる
   def authenticate_user
     return if @current_user
 
     flash[:notice] = "ログインが必要です"
+    store_location # 希望のurlを保存しておく
     redirect_to "/login"
   end
 
+  # 既にログイン済みのユーザがユーザ作成を行う場合に、ユーザページへリダイレクトさせる
   def forbid_login_user
     return unless @current_user
 
     flash[:notice] = "すでにログインしています"
     redirect_to @current_user
+  end
+
+  # 自分以外のユーザの情報を編集しようとした場合、自身のページにリダイレクトさせる
+  def ensure_correct_user
+    return if @current_user.id == params[:id].to_i
+
+    flash[:notice] = "権限がありません"
+    redirect_to @current_user
+  end
+
+  # 管理者権限がない場合、topページにリダイレクトさせる
+  def admin_user
+    redirect_to "/" unless current_user.admin?
   end
 end
