ba2c56c8ed5a14aafd061a2275687a9b4eb72af3
kei-nakano <keisukenever4get@gmail.com>
2020-07-26 13:48:52 +0900

    featureテスト追加

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -10,8 +10,17 @@ RSpec.feature "Items", type: :feature do
     click_link "ログイン"
     fill_in "email", with: user.email
     fill_in "password", with: "password"
+
+    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({ user_id: user.id })
     click_button "ログイン"
-    # click_link "商品投稿"
+    click_link "商品投稿"
+    expect(page).to have_content "商品名"
+    find('button.category-new-link').click
+    # click_button "新規作成する"
+    # expect(page).to have_content "投稿する"
+
+    # uri = URI.parse(current_url)
+    # expect("#{uri.path}?#{uri.query}").to eq("")
     # expect do
     #  click_link "New Project"
     #  fill_in "Name", with: "Test Project"
@@ -22,10 +31,5 @@ RSpec.feature "Items", type: :feature do
     #  expect(page).to have_content "Test Project"
     #  expect(page).to have_content "Owner: #{user.name}"
     # end.to change(Item.all, :count).by(1)
-  end
-
-  scenario "guest adds a project" do
-    visit "/"
-    click_link "ログイン"
   end
 end
