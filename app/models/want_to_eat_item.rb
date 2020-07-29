ba5dffbf455e55c9cab811fa9316e357c4396656
kei-nakano <keisukenever4get@gmail.com>
2020-07-29 21:05:21 +0900

    models/want_to_eat_itemテストの追加

--- app/models/want_to_eat_item.rb
+++ app/models/want_to_eat_item.rb
@@ -1,7 +1,7 @@
 class WantToEatItem < ApplicationRecord
   validates :user_id, presence: true
   validates :item_id, presence: true
-  validates :user_id, uniqueness: { scope: :item_id }
+  validates :item_id, uniqueness: { scope: :user_id }
   belongs_to :user
   belongs_to :item
 end
