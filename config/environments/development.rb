2cefbfd299b24e2fd96885cad129e1e2fed576c9
kei-nakano <keisukenever4get@gmail.com>
2020-08-04 13:58:54 +0900

    errorsコントローラを追加し、本番固有のerrorsテンプレートを追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports.
+  # エラーの詳細画面(赤画面)を表示するか
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
