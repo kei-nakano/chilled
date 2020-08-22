45acf24079a8b548030226fd27e35a4be3668c3b
kei-nakano <keisukenever4get@gmail.com>
2020-08-22 23:22:34 +0900

    keyとtokenの取得

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,35 +1,62 @@
 class LinebotController < ApplicationController
   require 'line/bot'
 
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = Rails.application.credentials.line_secret_key
+      config.channel_token = Rails.application.credentials.line_access_token
+    }
+  end
+
   def callback
     body = request.body.read
+
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    error 400 do 'Bad Request' end unless client.validate_signature(body, signature)
+    head :bad_request unless client.validate_signature(body, signature)
+
     events = client.parse_events_from(body)
 
-    events.each do |event|
+    events.each { |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
+          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
+          if event.message['text'].eql?('アンケート')
+            # private内のtemplateメソッドを呼び出します。
+            client.reply_message(event['replyToken'], template)
+          end
         end
       end
-      client.reply_message(event['replyToken'], message)
-    end
+    }
+
     head :ok
   end
 
   private
 
-  # LINE Developers登録完了後に作成される環境変数の認証
-  def client
-    @client ||= Line::Bot::Client.new do |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    end
+  def template
+    {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+        "type": "confirm",
+        "text": "今日のもくもく会は楽しいですか？",
+        "actions": [
+          {
+            "type": "message",
+            # Botから送られてきたメッセージに表示される文字列です。
+            "label": "楽しい",
+            # ボタンを押した時にBotに送られる文字列です。
+            "text": "楽しい"
+          },
+          {
+            "type": "message",
+            "label": "楽しくない",
+            "text": "楽しくない"
+          }
+        ]
+      }
+    }
   end
 end
